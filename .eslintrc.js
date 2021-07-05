99fbce249cf4edea85706bb0ce21eab5fe26135b
tomon9086 <tomon9086@gmail.com>
2021-07-05 17:04:43 +0900

    disable react/prop-types rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,7 +35,8 @@ module.exports = {
     'react/jsx-filename-extension': [2, {
       extensions: ['.js', '.jsx', '.ts', '.tsx']
     }],
-    'react/jsx-sort-props': 'error'
+    'react/jsx-sort-props': 'error',
+    'react/prop-types': 'off'
   },
   settings: {
     react: {
